6687a202a88291062125c5d6159ccd7b1cfcdbdd
nagitoru <yokuha188ku@gmail.com>
2020-09-29 11:04:13 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英字と数字を両方とも含んでください'
+
+  with_options presence: true do
+    validates :birthday
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は漢字・ひらがな・カタカナのいずれかで入力してください' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は漢字・ひらがな・カタカナのいずれかで入力してください' }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力してください' }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力してください' }
+  end
 end
